e09e10f0cadac09d0b50b9d499463fb6e6064d0f
Tom Siwik <tomas.sivicki@gmail.com>
2020-12-13 17:47:00 +0100

    chore: remove unused expression

--- flareact.config.js
+++ flareact.config.js
@@ -2,8 +2,6 @@ const path = require("path");
 
 module.exports = {
   webpack: (config, { dev, isWorker, defaultLoaders, webpack }) => {
-    const rules = config.module.rules;
-
     config.entry = path.resolve(__dirname, "src", "index.js");
 
     return config;
